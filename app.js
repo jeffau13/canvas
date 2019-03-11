6badf6975220d9d53cb09290dd43877b93f385ac
jeffau13 <jeffau13@gmail.com>
2019-03-10 21:51:32 -0400

    changed stroke style

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -9,32 +8,47 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 100;
 
-//flag to see if 
+//flag to see if
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
+let direction = true;
 
-function draw(e){
-    if(!isDrawing) return;// stop function from running if mouse is not down.
-    console.log(e);
-    hue= Math.floor(Math.random()*365);
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
-    ctx.beginPath();
-    //Starts from 
-    ctx.moveTo(lastX,lastY);
-    //Go to:
-    ctx.lineTo(e.offsetX,e.offsetY);
-    ctx.stroke();
-    [lastX, lastY] = [e.offsetX, e.offsetY];
+function draw(e) {
+  if (!isDrawing) return; // stop function from running if mouse is not down.
+
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+
+  //Starts from
+  ctx.moveTo(lastX, lastY);
+  //Go to:
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  
+  hue++;
+
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+
+  if(direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 }
-
-canvas.addEventListener('mousemove',draw);
-canvas.addEventListener('mousedown', (e)=> 
-    {isDrawing = true;
-        //update X and Y:
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-    }); 
-canvas.addEventListener('mouseup', ()=> isDrawing = false); 
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', e => {
+  isDrawing = true;
+  //update X and Y:
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => (isDrawing = false));
 //When mouse is out of window:
-canvas.addEventListener('mouseout', ()=> isDrawing = false); 
+canvas.addEventListener('mouseout', () => (isDrawing = false));
