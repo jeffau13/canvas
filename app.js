0065728fa3d88e1a1db38dabc5b75a409491e2b9
jeffau13 <jeffau13@gmail.com>
2019-03-10 21:36:11 -0400

    added drawable lines in draw function and event listener

--- app.js
+++ app.js
@@ -16,9 +16,21 @@ let lastY = 0;
 function draw(e){
     if(!isDrawing) return;// stop function from running if mouse is not down.
     console.log(e);
+    ctx.beginPath();
+    //Starts from 
+    ctx.moveTo(lastX,lastY);
+    //Go to:
+    ctx.lineTo(e.offsetX,e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
 canvas.addEventListener('mousemove',draw);
-canvas.addEventListener('mousedown', ()=> isDrawing = true); 
+canvas.addEventListener('mousedown', (e)=> 
+    {isDrawing = true;
+        //update X and Y:
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    }); 
 canvas.addEventListener('mouseup', ()=> isDrawing = false); 
+//When mouse is out of window:
 canvas.addEventListener('mouseout', ()=> isDrawing = false); 
